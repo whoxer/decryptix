81e311b08b1ce30c3b11693c9ffdf32dab7a84ac
whoxer <natanaelvieira1@hotmail.com>
2023-08-12 17:58:05 -0300

    Classe Game chamada na função main(); jogo inicia e se repete com os métodos definidos

--- Main.cpp
+++ Main.cpp
@@ -1,9 +1,27 @@
 #include "Game.hpp"
 
 int main() {
-    Game theGame;
+    bool playAgain = true;
 
-    // theGame.Play();
+	cout << "< Decryptix - v0.5.14 >" << endl;
+	cout << "> Há duas maneiras de jogar Decryptix: ";
+	cout << "você pode adivinhar um padrão que eu criar ou criar o seu padrão." << endl;
+
+    while (playAgain) {
+        char choice = ' ';
+        Game theGame;
+
+        theGame.Play();
+        cout << "\n A resposta: ";
+        theGame.Display(theGame.GetSolution());
+        cout << "\n\n" << endl;
+
+        while (choice != 's' && choice != 'n') {
+            cout << "\nJogar novamente (s/n): ";
+            cin >> choice;
+        }
+        playAgain = choice == 's' ? true : false;
+    }
 
     return 0;
 }
